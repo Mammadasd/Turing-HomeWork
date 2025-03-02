e5ada8e489ab5a5bc5a5f37ee5e85efc6dc39e89
Məmmədəli Şabanov <154734840+Mammadasd@users.noreply.github.com>
2025-03-03 01:38:56 +0400

    Commit

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-
-//        String input=SequenceCalculator.stringInput();
-        int result= SequenceCalculator.computeSequence(SequenceCalculator.stringInput());
-        System.out.println(result);
-
-    }
-}

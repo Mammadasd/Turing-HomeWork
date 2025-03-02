8b3ce3cb1783436eaf5ebea20477959bff8d3bbd
Məmmədəli Şabanov <154734840+Mammadasd@users.noreply.github.com>
2025-03-03 01:28:10 +0400

    Main.java faylı Test.java olaraq dəyişdirildi və düzəlişlər edildi

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,8 @@
-public class Main {
+public class Test {
     public static void main(String[] args) {
 
 //        String input=SequenceCalculator.stringInput();
-        int result=SequenceCalculator.computeSequence(SequenceCalculator.stringInput());
+        int result= SequenceCalculator.computeSequence(SequenceCalculator.stringInput());
         System.out.println(result);
 
     }
